cedd86decfd1e9aa5bd93035e5ce19a312098400
JosielCM <93870159+josielcm@users.noreply.github.com>
2025-04-09 03:49:44 +0000

    feat: Add support for cakefever configuration in FileManager

--- src/main/java/me/josielcm/event/FileManager.java
+++ src/main/java/me/josielcm/event/FileManager.java
@@ -13,19 +13,25 @@ public class FileManager {
     @Getter
     static Config settings = null;
 
+    @Getter
+    static Config cakefever = null;
+
     public static void loadFiles() {
         settings = new Config(plugin, "settings.yml");
         messages = new Config(plugin, "messages.yml");
+        cakefever = new Config(plugin, "cakefever.yml", "games");
     }
 
     public static void saveFiles() {
         settings.saveData();
         messages.saveData();
+        cakefever.saveData();
     }
 
     public static void reload() {
         settings = new Config(plugin, "settings.yml");
         messages = new Config(plugin, "messages.yml");
+        cakefever = new Config(plugin, "cakefever.yml", "games");
     }
 
     public static void debug() {
